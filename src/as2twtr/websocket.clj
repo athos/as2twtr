9f40291c6ec3d95ca6d5938a4fd16e892ff8a084
OHTA Shogo <athos0220@gmail.com>
2011-03-03 17:54:27 +0900

    refactoring websocket.clj

--- src/as2twtr/websocket.clj
+++ src/as2twtr/websocket.clj
@@ -6,6 +6,39 @@
           [java.security MessageDigest]
           [java.nio ByteBuffer]))
 
+(declare
+  make-websocket-client
+  disconnect
+  string->bytes
+  bytes->string
+  send-frame
+  recv-frame
+  handshake
+  call-with-websocket-client)
+
+;; websocket client
+(defrecord WebSocketClient [socket uri in out])
+
+(defn- websocket-port [uri]
+  (or (.getPort uri)
+      (case (.getScheme uri)
+	"ws" 80
+	"wss" 443
+	(java.net.MalformedURLException.))))
+
+(defn make-websocket-client [uri]
+  (let [uri (if (string? uri) (URI. uri) uri)
+	port (websocket-port uri)]
+    (let [socket (Socket. (.getHost uri) port)]
+      (WebSocketClient. socket
+			uri
+			(BufferedInputStream. (.getInputStream socket))
+			(BufferedOutputStream. (.getOutputStream socket))))))
+
+(defn disconnect [client]
+  (.close (:socket client)))
+
+;; string <-> byte array conversion
 (defn string->bytes [s]
   (.getBytes s "UTF-8"))
 
@@ -15,7 +48,7 @@
    (String. bs 0 size "UTF-8")))
 
 ;; functions for sending/receiving frames
-(defn send-data [out data]
+(defn- send-data [out data]
   (cond (string? data)
         #_=> (.write out (string->bytes data))
         (integer? data)
@@ -27,31 +60,33 @@
         #_=> (.write out data))
   (.flush out))
 
-(defn send-frame [out data]
-  (send-data out 0)
-  (send-data out data)
-  (send-data out 255))
+(defn send-frame [client data]
+  (doto (:out client)
+    (send-data 0)
+    (send-data data)
+    (send-data 255))
+  nil)
 
-(defn int->byte [x]
+(defn- int->byte [x]
   (let [MAX Byte/MAX_VALUE]
     (byte (or (and (<= x MAX) x)
               (- x (* 2 (+ MAX 1)))))))
 
-(defn recv-until [in pred]
+(defn- recv-until [in pred]
   (loop [bs []]
     (let [b (.read in)]
       (if (or (= b -1) (pred b))
         (into-array Byte/TYPE bs)
         (recur (conj bs (int->byte b)))))))
 
-(defn recv-frame [in]
-  (when-not (= (.read in) 0)
+(defn recv-frame [client]
+  (when-not (= (.read (:in client)) 0)
     ;; FIXME: more appropriate exception should be thrown
     (throw (Exception. "frame without \\x00 prefix received")))
-  (recv-until in #(= % 255)))
+  (recv-until (:in client) #(= % 255)))
 
 ;; handshake
-(defn handshake-request-header [^URI uri, key1, key2]
+(defn- handshake-request-header [^URI uri, key1, key2]
   (let [path (str (.getRawPath uri)
                   (if (.getRawQuery uri)
                     (str \? (.getRawQuery uri))
@@ -64,17 +99,17 @@
                "Sec-WebSocket-Key1: " key1 "\r\n"
                "Sec-WebSocket-Key2: " key2 "\r\n")))
 
-(defn make-key []
+(defn- make-key []
   (let [x (long (rand (Math/pow 2 32)))
         y (inc (rand-int 12))]
     [(* (quot x y) y) y]))
 
-(defn make-body []
+(defn- make-body []
   (into-array Byte/TYPE
               (map (fn [_] (int->byte (rand-int 256)))
                    (range 8))))
 
-(defn encode [key ns]
+(defn- encode [key ns]
   (let [chars (str "!\"#$%&'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRST"
                    "UVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~")]
     (letfn [(random-char []
@@ -96,18 +131,21 @@
         (join (merge `(~start ~@(merge mid (repeat ns \space)) ~end)
                      (random-chars)))))))
 
-(defn send-handshake-request [uri out]
+(defn- send-handshake-request [client]
   (let [[key1 ns1] (make-key)
         [key2 ns2] (make-key)
         body (make-body)
-        header (handshake-request-header uri (encode key1 ns1) (encode key2 ns2))]
-    (send-data out header)
-    (send-data out "\r\n")
-    (send-data out body)
+        header (handshake-request-header (:uri client)
+					 (encode key1 ns1)
+					 (encode key2 ns2))]
+    (doto (:out client)
+      (send-data header)
+      (send-data "\r\n")
+      (send-data body))
     [(/ key1 ns1) (/ key2 ns2) body]))
 
-(defn recv-handshake-response [in]
-  (letfn [(recv-fields []
+(defn- recv-handshake-response [client]
+  (letfn [(recv-fields [in]
             (loop [fields []]
               (let [field (bytes->string (recv-until in #(= % (int \return))))]
                 (.read in)  ; omit a newline
@@ -120,14 +158,14 @@
                         (conj m [(lower-case name) value])))
                     {}
                     fields))]
-    (let [fields (recv-fields)
+    (let [fields (recv-fields (:in client))
           body (byte-array 16)]
-      (when-not (= (.read in body) 16)
+      (when-not (= (.read (:in client) body) 16)
         ;; FIXME
         (throw (Exception.)))
       [(fields->map (rest fields)) body])))
 
-(defn challenge-response [key1 key2 body]
+(defn- challenge-response [key1 key2 body]
   (let [buf (ByteBuffer/allocate 16)
         md5 (MessageDigest/getInstance "MD5")]
     (doto buf
@@ -137,48 +175,40 @@
     (.update md5 (.array buf))
     (.digest md5)))
 
-(defn handshake [uri in out]
-  (let [[key1 key2 body] (send-handshake-request uri out)
+(defn handshake [client]
+  (let [[key1 key2 body] (send-handshake-request client)
         answer (challenge-response key1 key2 body)
-        [header body] (recv-handshake-response in)]
+        [header body] (recv-handshake-response client)]
     (when-not (= answer body)
       ;; FIXME
       (Exception.))
     header))
 
-;; sockets and connections
-(defn make-websocket-socket [uri]
-  (let [port (or (.getPort uri)
-                 (case (.getScheme uri)
-                   "ws"  80
-                   "wss" 443
-                   (java.net.MalformedURLException.)))]
-    (Socket. (.getHost uri) port)))
-
-(defn with-connection [uri proc]
-  (let [uri (if (string? uri) (URI. uri) uri)]
-    (with-open [socket (make-websocket-socket uri)
-                in (BufferedInputStream. (.getInputStream socket))
-                out (BufferedOutputStream. (.getOutputStream socket))]
-      (try 
-        (handshake uri in out)
-        (proc in out)
-        ;; FIXME: more appropriate exception handling
-        (catch Exception e
-          (throw e))))))
+;; convenient API
+(defn call-with-websocket-client [uri proc]
+  (let [client (make-websocket-client uri)]
+    (try
+      (handshake client)
+      (proc client)
+      ;; FIXME: more appropriate exception handling
+      (catch Exception e
+	(throw e))
+      (finally
+        (when-not (.isClosed (:socket client))
+	  (disconnect client))))))
 
 (comment
 (letfn [(prompt [p]
           (print p)
           (flush)
           (read-line))]
-  (with-connection
+  (calll-with-websocket-client
     "ws://localhost:8080/"
-    (fn [in out]
+    (fn [client]
       (loop []
         (let [line (prompt "> ")]
           (when line
-            (send-frame out line)
-            (println (bytes->string (recv-frame in)))
+            (send-frame client line)
+            (println (bytes->string (recv-frame client)))
             (recur)))))))
 )
