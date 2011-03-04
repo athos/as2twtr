e07a491099dd4ff25e6bfcb0d04c6df426d9b853
OHTA Shogo <athos0220@gmail.com>
2011-03-04 16:48:35 +0900

    refactor so that twitter.clj doesn't depend on config.clj

--- src/as2twtr/twitter.clj
+++ src/as2twtr/twitter.clj
@@ -3,33 +3,26 @@
   (import [twitter4j Twitter Paging]
 	  [twitter4j.http AccessToken]))
 
-(def *oauth-consumer-key* (config "twitter.consumer-key"))
-(def *oauth-consumer-secret* (config "twitter.consumer-secret"))
-
-(def *oauth-access-token* (config "twitter.access-token"))
-(def *oauth-access-token-secret* (config "twitter.access-token-secret"))
-
-(def twitter
+(defn make-twitter-client [consumer-key consumer-secret access-token access-token-secret]
   (doto (Twitter.)
-    (.setOAuthConsumer *oauth-consumer-key* *oauth-consumer-secret*)
-    (.setOAuthAccessToken (AccessToken. *oauth-access-token*
-					*oauth-access-token-secret*))))
+    (.setOAuthConsumer consumer-key consumer-secret)
+    (.setOAuthAccessToken (AccessToken. access-token access-token-secret))))
 
-(defn update-status [msg]
-  (.updateStatus twitter msg))
+(defn update-status [client msg]
+  (.updateStatus client msg))
 
-(defn in-reply-to [status]
+(defn in-reply-to [client status]
   (let [reply-id (.getInReplyToStatusId status)]
     (if (= reply-id -1)
       nil
-      (-> twitter (.showStatus reply-id) (.getText)))))
+      (-> client (.showStatus reply-id) (.getText)))))
 
 (defn mentions
-  ([] (mentions nil))
-  ([since-id]
+  ([client] (mentions client nil))
+  ([client since-id]
    (let [ms (if since-id
-	      (.getMentions twitter (Paging. (long since-id)))
-	      (.getMentions twitter))]
+	      (.getMentions client (Paging. (long since-id)))
+	      (.getMentions client))]
      (sort #(>= (first %1) (first %2))
 	   (for [m ms]
 	     (let [u (.getUser m)]
@@ -39,16 +32,8 @@
 		(.getId u)
 		(in-reply-to m)]))))))
 
-(defn followers []
-  (-> (.getFriendsIDs twitter) (.getIDs) seq))
+(defn followers [client]
+  (-> client (.getFriendsIDs) (.getIDs) seq))
 
 (defn max-status-id [ms]
   (if (empty? ms) false (first (first ms))))
-
-(defn feed-to-twitter [name id time msg]
-  (let [content (format "%s: %s" name msg)
-	id:time (format "(%s:%s)" id time)]
-    (update-status
-     (if (> (+ (count content) (count id:time)) 139)
-       (format "%s\u2026 %s" (subs content 0 (- 138 (count id:time))) id:time)
-       (format "%s %s" content id:time)))))
