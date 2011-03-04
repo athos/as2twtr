9f2700d578b212251b6bf6a3e826d36e146f36a0
OHTA Shogo <athos0220@gmail.com>
2011-03-04 19:15:54 +0900

    made config.clj less evil

--- src/as2twtr/config.clj
+++ src/as2twtr/config.clj
@@ -1,30 +1,17 @@
 (ns as2twtr.config)
 
-(def *configuration-filename* (atom "as2twtr.conf"))
-
-(def *configurations* (atom {}))
+(def *configurations* (ref nil))
 
 (defmacro defconfig [name & key-values]
   (let [configs (map (fn [[key value]]
 		       [(str name "." key) value])
 		     key-values)]
-    `(do (swap! *configurations*
+    `(do (alter *configurations*
 		(fn [~'x] (into ~'x '~configs)))
 	 nil)))
 
-(def already-loaded? (ref false))
-
-(defn set-config-file! [filename]
-  (reset! *configuration-filename* filename))
-
-(defn config [key]
+(defn read-configuration [filename]
   (dosync
-   (when-not @already-loaded?
-     (load-file @*configuration-filename*)
-     (ref-set already-loaded? true)))
-  (@*configurations* key))
-
-(defn reload []
-  (dosync
-   (ref-set already-loaded? false))
-  (load-file @*configuration-filename*))
+    (ref-set *configurations* {})
+    (load-file filename)
+    @*configurations*))
