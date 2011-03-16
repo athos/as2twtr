21602caebc2da59e70fb9df13abf657f8890af0e
OHTA Shogo <athos0220@gmail.com>
2011-03-16 19:38:29 +0900

    added login & post APIs for AS chat

--- src/as2twtr/as_chat.clj
+++ src/as2twtr/as_chat.clj
@@ -1,11 +1,13 @@
 (ns as2twtr.as-chat
-  (use [clojure.contrib.json :only (read-json)])
+  (use [as2twtr.http :only (http-request)]
+       [clojure.contrib.json :only (read-json)])
   (require [as2twtr.websocket :as ws]))
 
 (declare
   make-chat-client
   connect
   disconnect
+  login
   post-message
   get-message
   )
@@ -21,7 +23,7 @@
   (keyword (:event message)))
 
 ;; chat client
-(defrecord ChatClient [websocket post-uri observer-thread])
+(defrecord ChatClient [user password websocket api-base-uri observer-thread])
 
 (defn- make-handler [client observer]
   (let [handle-it (or observer (fn [_ _] nil))]
@@ -32,12 +34,15 @@
       (doto (Thread. iter)
 	(.start)))))
 
-(defn connect [websocket-uri post-uri & [observer]]
-  (let [client (ChatClient. (ws/make-websocket-client websocket-uri)
-			    post-uri
+(defn connect [user password room-id websocket-uri api-base-uri & [observer]]
+  (let [client (ChatClient. user
+			    password
+			    (ws/make-websocket-client websocket-uri)
+			    api-base-uri
 			    (atom nil))]
     (ws/handshake (:websocket client))
-    (reset! (:observer-thread client) (make-handler client observer))
+    (when observer
+      (reset! (:observer-thread client) (make-handler client observer)))
   client))
 
 (defn disconnect [client]
@@ -46,9 +51,26 @@
   (ws/disconnect (:websocket client))
   nil)
 
+(defn- api-request [client path & {query :query, body :body}]
+  (let [base-uri (:api-base-uri client)]
+    (if body
+      (http-request (str base-uri path)
+	:method :post
+	:options query
+	:body body)
+      (http-request (str base-uri path) :options query))))
+
+;; login
+(defn login [client]
+  (api-request
+    client "/login"
+    :query {:user (:user client), :password (:password client)}))
+
 ;; post message
 (defn post-message [client message]
-  nil)
+  (api-request
+    client "/message"
+    :body {:room_id (:room-id client), :message message}))
 
 ;; get message (synchronously)
 (defn get-message [client]
