f29082689aca00a97be96528ba0417131b816903
OHTA Shogo <athos0220@gmail.com>
2011-03-16 18:38:47 +0900

    modified APIs for SSL

--- src/as2twtr/ssl.clj
+++ src/as2twtr/ssl.clj
@@ -1,31 +1,91 @@
 (ns as2twtr.ssl
-  (import [javax.net.ssl X509TrustManager SSLContext]))
+  (use [clojure.contrib.import-static :only (import-static)])
+  (import [javax.net.ssl X509TrustManager SSLContext HostnameVerifier]))
+
+(import-static javax.net.ssl.HttpsURLConnection
+  getDefaultSSLSocketFactory
+  setDefaultSSLSocketFactory
+  getDefaultHostnameVerifier
+  setDefaultHostnameVerifier)
 
 (declare
-  make-ssl-socket-factory
+  SSL_DEFAULT_CONTEXT
+  SSL_NONCERTIFIED_CONTEXT
+  call-with-ssl-context
+  with-ssl-context
   make-ssl-socket
   )
 
-(defn- make-nonchecking-trust-manager [& {client-checker :client-checker
-					  server-checker :server-checker
-					  accepted-issuers :accepted-issuers}]
-  (let [client-checker (or client-checker (fn [_ _] nil))
-	server-checker (or server-checker (fn [_ _] nil))
-	accepted-issuers (or accepted-issuers (fn [] nil))]
-    (reify X509TrustManager
-      (checkClientTrusted [this certificates auth-type]
-	(client-checker certificates auth-type))
-      (checkServerTrusted [this certificates auth-type]
-	(server-checker certificates auth-type))
-      (getAcceptedIssuers [this]
-	(accepted-issuers)))))
+(def SSL_DEFAULT_CONTEXT {:client-checker nil, :server-checker nil
+			  :accepted-issuers nil, :hostname-verifier nil})
+(def SSL_NONCERTIFIED_CONTEXT {:client-checker (fn [_ _] nil)
+			       :server-checker (fn [_ _] nil)
+			       :accepted-issuers (fn [] nil)
+			       :hostname-verifier (fn [_ _] true)})
 
-(defn make-ssl-socket-factory [& opts]
-  (let [tmanager (apply make-nonchecking-trust-manager opts)
-	context (SSLContext/getInstance "SSL")]
-    (.init context nil (into-array [tmanager]) nil)
+(def #^{:private true} *ssl-context* SSL_DEFAULT_CONTEXT)
+
+(defn- make-trust-manager []
+  (reify X509TrustManager
+    (checkClientTrusted [this certificates auth-type]
+      ((:client-checker *ssl-context*) certificates auth-type))
+    (checkServerTrusted [this certificates auth-type]
+      ((:server-checker *ssl-context*) certificates auth-type))
+    (getAcceptedIssuers [this]
+      ((:accepted-issuers *ssl-context*)))))
+
+(defn- make-socket-factory []
+  (let [context (SSLContext/getInstance "SSL")]
+    (.init context nil (into-array [(make-trust-manager)]) nil)
     (.getSocketFactory context)))
 
-(defn make-ssl-socket [host port & [factory]]
-  (let [factory (or factory (make-ssl-socket-factory))]
+(defn- make-hostname-verifier []
+  (reify HostnameVerifier
+    (verify [this hostname session]
+      ((:hostname-verifier *ssl-context*) hostname session))))
+
+(defn call-with-ssl-context [context proc]
+  (let [{client-checker :client-checker
+	 server-checker :server-checker
+	 accepted-issuers :accepted-issuers
+	 hostname-verifier :hostname-verifier}
+	context
+	checkers? (and client-checker server-checker accepted-issuers)
+	verifier? (not (not hostname-verifier))]
+    (when (and (not checkers?)
+	       (or client-checker server-checker accepted-issuers))
+      (throw (Exception. (str "if one of :client-checker, :server-checker, "
+			      "or :accepted-issuer is specified, then all of"
+			      "them must be specified"))))
+    (binding [*ssl-context* context]
+      (let [old-factory (getDefaultSSLSocketFactory)
+	    old-verifier (getDefaultHostnameVerifier)]
+	(try
+	  (when checkers?
+	    (setDefaultSSLSocketFactory (make-socket-factory)))
+	  (when verifier?
+	    (setDefaultHostnameVerifier (make-hostname-verifier)))
+	  (proc)
+	  (finally
+	   (when checkers?
+	     (setDefaultSSLSocketFactory old-factory))
+	   (when verifier?
+	     (setDefaultHostnameVerifier old-verifier))))))))
+
+(defmacro with-ssl-context
+  "Evaluates exprs in the specified SSL context.
+
+   Example:
+      (with-ssl-context SSL_NONCERTIFIED_CONTEXT
+        (.openConnection (java.net.URL. \"https://www.example.com\")))
+
+   This makes it possible to connect to non-certified servers via SSL.
+
+   Note: This macro isn't thread-safe. Use locks for mutual exclusion."
+  [context & body]
+  `(call-with-ssl-context ~context
+     (fn [] ~@body)))
+
+(defn make-ssl-socket [host port]
+  (let [factory (getDefaultSSLSocketFactory)]
     (.createSocket factory host port)))
